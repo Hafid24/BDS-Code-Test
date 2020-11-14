bde65e37d8d6baa7081e53ceffea59de74d9ef50
hafid24 <ilymassh@gmail.com>
2020-11-14 22:23:11 +0100

    validating req fields

--- server/routes/api/register.js
+++ server/routes/api/register.js
@@ -2,64 +2,97 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const userQueries = require("../../Queries/userQueries");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const { username, usertype, firstname, lastname, email, password } = req.body;
-  try {
-    let user = await userQueries.findUserByEmail(email);
-
-    if (user.rows.length > 0) {
-      return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+router.post(
+  "/",
+  [
+    check("password")
+      .not()
+      .isEmpty(),
+    check("usertype", "Type is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("username", "Please enter a username with 6 or more characters")
+      .matches(/^[a-zA-Z0-9_.]*$/, "i")
+      .isLength({ max: 20 })
+      .not()
+      .isEmpty(),
+    check("firstname").isLength({ max: 100 }),
+    check("lasttname").isLength({ max: 100 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-
-    const allUsers = await userQueries.getUsers();
-    const adminExistance = allUsers.rows.every(
-      (user) => user.usertype != "admin"
-    );
-
-    if (!adminExistance && usertype == "admin") {
-      return res.status(400).json({ errors: [{ msg: "Admin Exists" }] });
-    }
-
-    newUser = {
+    const {
       username,
       usertype,
       firstname,
       lastname,
       email,
       password
-    };
+    } = req.body;
+    try {
+      let user = await userQueries.findUserByEmail(email);
 
-    const salt = await bcrypt.genSalt(10);
-    newUser.password = await bcrypt.hash(password, salt);
-
-    const id = await userQueries.createUser(newUser);
-
-    const payload = {
-      user: {
-        id
+      if (user.rows.length > 0) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
-    };
 
-    jwt.sign(
-      payload,
-      config.get("jwtSecret"),
-      { expiresIn: config.get("tokenExpiration") },
-      (err, token) => {
-        if (err) {
-          throw err;
+      const allUsers = await userQueries.getUsers();
+      const adminExistance = allUsers.rows.every(
+        (user) => user.usertype != "admin"
+      );
+
+      if (!adminExistance && usertype == "admin") {
+        return res.status(400).json({ errors: [{ msg: "Admin Exists" }] });
+      }
+
+      newUser = {
+        username,
+        usertype,
+        firstname,
+        lastname,
+        email,
+        password
+      };
+
+      const salt = await bcrypt.genSalt(10);
+      newUser.password = await bcrypt.hash(password, salt);
+
+      const id = await userQueries.createUser(newUser);
+
+      const payload = {
+        user: {
+          id
         }
-        console.log("finish");
-        res.json({ token });
-      }
-    );
-  } catch (error) {
-    res.status(500).send("Server error");
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: config.get("tokenExpiration") },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          console.log("finish");
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      res.status(500).send("Server error");
+    }
   }
-});
+);
 
 module.exports = router;
