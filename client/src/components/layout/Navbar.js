ae66326d0d5bdb7735667e753b9cc3ac4278a77a
hafid24 <ilymassh@gmail.com>
2020-11-15 20:35:16 +0100

    creating Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
+import { Box, Heading, Flex, Button } from "@chakra-ui/react";
 
-class Navbar extends Component {
-  state = {};
-  render() {
-    return <div>Hello</div>;
-  }
-}
+const Navbar = (props) => {
+  const [show, setShow] = React.useState(false);
+  const handleToggle = () => setShow(!show);
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      bg="teal.500"
+      color="white"
+      {...props}
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
+          BDS
+        </Heading>
+      </Flex>
+      <Box
+        display={{ sm: show ? "block" : "none", md: "block" }}
+        mt={{ base: 4, md: 0 }}
+      >
+        <Button bg="transparent" border="1px">
+          Create account
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
 
 export default Navbar;
